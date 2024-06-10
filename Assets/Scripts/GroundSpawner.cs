2a5bf1d3471a0c5450e1c9cf2e0c36835edd73ae
Sonit Bahl <sonitbahl@gmail.com>
2024-06-11 02:04:04 +0530

    added infinite ground spawn logic

---
+++ Assets/Scripts/GroundSpawner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GroundSpawner : MonoBehaviour {
+
+    [SerializeField] GameObject groundTile;
+    Vector3 nextSpawnPoint;
+
+    public void SpawnTile (bool spawnItems)
+    {
+        GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
+    }
+
+    public void SpawnTile ()
+    {
+        SpawnTile(false); // Default to false if no parameter is provided
+    }
+
+    private void Start () {
+        for (int i = 0; i < 15; i++) {
+            SpawnTile(); // Call the parameterless version
+        }
+    }
+}
